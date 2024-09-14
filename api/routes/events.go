c995921cf5b6474a5da1cbb823d47f8a7a1ceccc
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-14 00:55:53 -0500

    [Events] - Validate whether request matches userId on event and from request token claim to allow permission to update / delete

--- api/routes/events.go
+++ api/routes/events.go
@@ -63,9 +63,15 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = repository.GetEventById(eventId)
+	userId := context.GetInt64("userId")
+	event, err := repository.GetEventById(eventId)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
 		return
 	}
 
@@ -94,9 +100,15 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event, err := repository.GetEventById(eventId)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete event."})
 		return
 	}
 
