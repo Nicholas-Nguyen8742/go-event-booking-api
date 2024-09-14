3888d06823685262545eaf56d3932f7d8556298d
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-13 23:16:09 -0500

    [Users] - Hash password using utils package function to not store plain passwords to db

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"event-booking-api/storage"
+	"event-booking-api/utils"
 )
 
 type User struct {
@@ -23,7 +24,12 @@ func (user User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(user.Email, user.Password)
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(user.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
