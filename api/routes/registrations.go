676b6460b04bbe8cd36531bffa0dfe10d87006de
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-14 01:26:04 -0500

    [Register] - Add methods for register and cancel event booking route controllers

---
+++ api/routes/registrations.go
@@ -0,0 +1,51 @@
+package routes
+
+import (
+	"event-booking-api/repository"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could no parse event id."})
+		return
+	}
+
+	event, err := repository.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not getch event."})
+		return
+	}
+
+	err = event.Register(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user for event."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Registered!"})
+}
+
+func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could no parse event id."})
+		return
+	}
+
+	var event repository.Event
+	event.ID = eventId
+	err = event.Cancel(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user for event."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Cancelled registration for event successfully."})
+}
