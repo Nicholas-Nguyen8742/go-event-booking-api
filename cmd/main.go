58bfac845e5e3681af84002af89cfc0545959eec
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-13 22:51:57 -0500

    [Chore] - Refactor main.go of fluff, move event controllers out of main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"event-booking-api/repository"
+	"event-booking-api/api/routes"
 	"event-booking-api/storage"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,29 +11,7 @@ func main() {
 	storage.InitDb()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
-}
-
-func getEvents(context *gin.Context) {
-	events := repository.GetAllEvents()
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event repository.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	event.Save()
-
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
