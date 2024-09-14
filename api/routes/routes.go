b8f916153f39f5155ad0a73e8e1ad34150a6db43
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-14 00:49:08 -0500

    [Auth] - Implement protected api endpoints w/ auth middleware logic

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,13 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"event-booking-api/api/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	protectedRoutes := server.Group("/")
+	protectedRoutes.Use(middleware.Authenticate)
+	protectedRoutes.POST("/events", createEvent)
+	protectedRoutes.PUT("/events/:id", updateEvent)
+	protectedRoutes.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
