75487788cc6a2c124b93080070a8c4fb61cad319
Nicholas Nguyen <nicholasnguyen8742@gmail.com>
2024-09-13 22:54:46 -0500

    [Events] - Update CRUD repository functions related to events

--- repository/event_repository.go
+++ repository/event_repository.go
@@ -16,7 +16,7 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() error {
+func (event Event) Save() error {
 	query := `
 		INSERT INTO events(name, description, location, dateTime, user_id)
 		VALUES (?, ?, ?, ?, ?)
@@ -28,16 +28,94 @@ func (e Event) Save() error {
 	}
 
 	defer stmt.Close()
-	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	result, err := stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.UserID)
 	if err != nil {
 		return err
 	}
 
 	id, err := result.LastInsertId()
-	e.ID = id
+	event.ID = id
 	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+
+	rows, err := storage.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close();
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := `
+		SELECT * FROM events
+		WHERE id = ?
+	`
+
+	row, err := storage.DB.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	var event Event
+	err = row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
+
+func  (event Event) Update() error {
+	query := `
+		UPDATE events
+		SET name = ?, description = ?, location = ?, dateTime = ?
+		WHERE id = ?
+	`
+
+	stmt, err := storage.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	_, err = stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.ID)
+
+	return err
+}
+
+
+func (event Event) Delete() error {
+	query := `
+		DELETE FROM events WHERE id = ?
+	`
+
+	stmt, err := storage.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.ID)
+	return err
 }
